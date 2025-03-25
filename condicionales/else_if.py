42f6b1e3202305b314c3316b8483b3065dc57496
baileytorch <baileytorch@hotmail.com>
2025-03-25 16:11:41 -0300

    ultimos cambios condicionales

---
+++ condicionales/else_if.py
@@ -0,0 +1,16 @@
+edad = 49
+
+if edad >= 18:
+    print("Ud es mayor de edad.")
+else:
+    print("Ud NO es mayor de edad.")
+    
+ingreso_mensual = 500000
+gasto_mensual = 400000
+
+if ingreso_mensual - gasto_mensual > 10000:
+    print("Ud, tiene posibilidad de ahorrar")
+elif ingreso_mensual - gasto_mensual < 50000:
+    print("Cuidado con las luquitas...")
+else:
+    print("Ud. est{a en la línea de pobreza")
